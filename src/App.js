2061cfd8099c32654c2c6b6f7ea5dc9592237802
AhmadJaber <jaber131706@gmail.com>
2020-06-03 18:17:21 +0600

    used hooks in functional components, created custom reusable hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div className='app'>
       <h1>Adopt Me!</h1>
-      <Pet name='Luna' animal='Dog' breed='Havanese' />
-      <Pet name='Putu' animal='Cat' breed='Stray' />
-      <Pet name='Meg' animal='Lion' breed='African' />
+      <SearchParams />
     </div>
   );
 };
